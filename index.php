18ecd801bdcde3a7f8a54deec28ae02f647c4f06
Jonathan Ham <jham@infolink.com>
2013-05-07 10:28:36 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
 			<p>Housing</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $h_id ?>">Housing Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $h_id ?>">Housing Section &raquo;</a></p>
             </div>
           </div>
 
@@ -60,7 +60,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Food</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $f_id ?>">Food Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $f_id ?>">Food Section &raquo;</a></p>
             </div>
           </div>
 
@@ -76,7 +76,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Children</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $c_id ?>">Children Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $c_id ?>">Children Section &raquo;</a></p>
             </div>
           </div>
 
@@ -92,7 +92,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Employment</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $e_id ?>">Employment and Education Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $e_id ?>">Employment and Education Section &raquo;</a></p>
             </div>
           </div>
 
@@ -108,7 +108,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Money</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $m_id ?>">Money Management Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $m_id ?>">Money Management Section &raquo;</a></p>
             </div>
           </div>
 
@@ -124,7 +124,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Health and Wellness</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $hw_id ?>">Health and Wellness Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $hw_id ?>">Health and Wellness Section &raquo;</a></p>
             </div>
           </div>
 
@@ -140,7 +140,7 @@ require('header.php');
             </a>
             <div class="carousel-caption">
               <p>Transportation</p>
-              <p><a style="color:#000000;" href="page.php?id=<?php echo $t_id ?>">Transportation Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $t_id ?>">Transportation Section &raquo;</a></p>
             </div>
           </div>
 
