f83a8454c3fbc848783d469851a68d8d143a473a
Jonathan Ham <jham@infolink.com>
2013-05-07 09:57:02 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ require('header-main.php');
             </a>
             <div class="carousel-caption">
 			<p>Housing</p>
-              <p><a href="page.php?id=<?php echo $h_id ?>">Housing Section &raquo;</a></p>
+              <p><a style="color:#FFFFFF;" href="page.php?id=<?php echo $h_id ?>">Housing Section &raquo;</a></p>
             </div>
           </div>
 
