57aade53107333baa4ab4765f41fb9b78a62de0c
Jonathan Ham <jham@infolink.com>
2013-05-07 11:35:06 -0400

    modified:   about.php 	modified:   contact.php 	modified:   includes/main.css 	modified:   page.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="includes/text-black.css" rel="stylesheet" type="text/css">
+<!--link href="includes/text-black.css" rel="stylesheet" type="text/css"-->
 <!-- <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
 -->
