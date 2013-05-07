b71763cdcb95cf81bf689f81ac04534ba4347690
Jonathan Ham <jham@infolink.com>
2013-05-07 10:51:19 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -168,7 +168,7 @@ require('header.php');
 
 ?>  
     <h1 class="center"><?php echo $name?></h1>
-		<div style="padding-bottom:30px;"><p><?php echo $text ?></p></div>
+		<div style="padding-bottom:30px; color:#FFFFFF;"><p><?php echo $text ?></p></div>
 <?php
 						}while($trow = mysqli_fetch_array($tresults));
 ?>		
