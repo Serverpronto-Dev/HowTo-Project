70db9c2e6efd1f358ebccea0a5bf5587ab88fce6
Jonathan Ham <jham@infolink.com>
2013-05-07 15:51:07 -0400

    modified:   header-test.php 	modified:   includes/main.css

--- header-test.php
+++ header-test.php
@@ -46,7 +46,7 @@ session_start();
         </a-->
 
 
-<table width="900px" border="0" cellpadding="10" cellspacing="0" class="center">
+<table width="900px" border="0" cellpadding="10" cellspacing="0" class="headerbanner">
   <tr>
     <td valign="top"><a href="http://www.serverpronto.com/"><img src="images/logo-portal.png" width="379" height="42" alt="ServerPronto HowTo"></a></td>
     <td align="right" valign="top"><span class="headerlinks">Sales: 1-877-24-PRONTO&nbsp;&nbsp;&nbsp;<a href"#" style="CURSOR: pointer" onClick="javascript:window.open('http://www.websitealive3.com/3606/rRouter.asp?groupid=3606&amp;websiteid=0&amp;departmentid=1905','guest','width=575,height=490');">Live Chat</a>&nbsp;&nbsp;&nbsp;</span></td>
