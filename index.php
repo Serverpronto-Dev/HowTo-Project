0eb388579fa82203476d10f5ee79e33899650c27
Jonathan Ham <jham@infolink.com>
2013-05-07 09:30:46 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<head class="headerback">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 
@@ -25,7 +25,7 @@ require('header-main.php');
 
 <body>
 
-<div class="container headerback">
+<div class="container">
 
 <!--  Carousel -->
       <!--  consult Bootstrap docs at 
