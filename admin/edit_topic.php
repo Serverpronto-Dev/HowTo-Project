a13ae7c5f25643738172ce2b242fde3973bb7b06
Jonathan Ham <jham@infolink.com>
2013-05-09 07:58:22 -0400

    modified:   admin/edit_topic.php

--- admin/edit_topic.php
+++ admin/edit_topic.php
@@ -19,6 +19,7 @@ include('../includes/db.php');
 
                 $topic_name = mysqli_real_escape_string($db, strip_tags( $_POST['topic_name']));
 				$id= mysqli_real_escape_string($db, strip_tags( $_POST['id']));
+				$description =  mysqli_real_escape_string($db, strip_tags( $_POST['description']));
 
 //Check that no category esists with this title
 $tresults = mysqli_query($db, "SELECT name FROM tbl_topic WHERE name='$topic_name'");
@@ -36,6 +37,9 @@ $tresults = mysqli_query($db, "SELECT name FROM tbl_topic WHERE name='$topic_nam
         if(empty($topic_name)){
                 $topic_error="You must specify a name.";
                 $c++;
+		}elseif(empty($description)){
+                $desc_error="Description cannot be empty.";
+                $c++;				
 //If valid insert data
         }else{ if($c==0){
 //Enter valid data into DB
@@ -56,6 +60,39 @@ $tresults = mysqli_query($db, "SELECT name FROM tbl_topic WHERE name='$topic_nam
                 }
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<script type="text/javascript" src="../includes/tinymce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        element_format : "html",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+
+        // Skin options
+        skin : "o2k7",
+        skin_variant : "silver",
+
+        // Drop lists for link/image/media/template dialogs
+        template_external_list_url : "js/template_list.js",
+        external_link_list_url : "js/link_list.js",
+        external_image_list_url : "js/image_list.js",
+        media_external_list_url : "js/media_list.js",
+
+});
+</script>
+</head>
 <body>
 <div class="container ">
 
@@ -73,9 +110,13 @@ $nresults = mysqli_query($db, "SELECT name FROM tbl_topic WHERE id='$id'");
         $nrow = mysqli_fetch_array($nresults);
         $topic_name=$nrow['name'];								
 		$id=$nrow['id'];								
+		$description=$nrow['description'];				
 ?>
-                                <td>New Topic Name:</td><td><input type="text" name="topic_name" value="<?php echo $topic_name ?>" size="85"><span class="red"><?php echo $topic_error ?></span></td>
+                                <td>New Topic Name:</td><td><input type="text" name="topic_name" value="<?php echo $topic_name ?>" size="85"></td><td><span class="red"><?php echo $topic_error ?></span></td>
                                 </tr>
+								<tr>
+                                <td>Description:</td><td><textarea name="description" cols="70" rows="25" Value="<?php echo $description ?>"><?php echo $description ?></textarea></td><td><span class="red"><?php echo $desc_error ?></span></td>
+                                </tr>								
                                 <tr>
                                 <td class="lastrow">
 								<input type="hidden" name="id" value="<?php echo $id ?>">
