6893f73ea12fe7585bd7c6c37862f2b8916ffc66
Jonathan Ham <jham@infolink.com>
2013-05-09 08:36:42 -0400

    modified:   admin/edit_cat.php

--- admin/edit_cat.php
+++ admin/edit_cat.php
@@ -20,9 +20,10 @@ include('../includes/db.php');
                 $cat_name = mysqli_real_escape_string($db, strip_tags( $_POST['cat_name']));
 				$id= mysqli_real_escape_string($db, strip_tags( $_POST['id']));
 				$topic_id= mysqli_real_escape_string($db, strip_tags( $_POST['topic_id']));
+				$description =  mysqli_real_escape_string($db, strip_tags( $_POST['description']));
 
 //Check that no category esists with this title
-$tresults = mysqli_query($db, "SELECT name FROM tbl_dept WHERE name='$cat_name'");
+$tresults = mysqli_query($db, "SELECT name FROM tbl_dept WHERE name='$cat_name' AND id!='$dept_id'");
         $trow = mysqli_fetch_array($tresults);
         $name_test=$trow['name'];
         if(!empty($name_test)){
@@ -37,11 +38,14 @@ $tresults = mysqli_query($db, "SELECT name FROM tbl_dept WHERE name='$cat_name'"
         if(empty($cat_name)){
                 $cat_error="You must specify an category name.";
                 $c++;
+		}elseif(empty($description)){
+                $desc_error="Description cannot be empty.";
+                $c++;				
 //If valid insert data
         }else{ if($c==0){
 //Enter valid data into DB
 
-        mysqli_query($db, "UPDATE tbl_dept SET name='$cat_name' WHERE id='$dept_id'");
+        mysqli_query($db, "UPDATE tbl_dept SET name='$cat_name', description='$description' WHERE id='$dept_id'");
                 mysqli_close($db);
                         header('Location: select_category.php?id='.$topic_id);
         }
@@ -58,6 +62,39 @@ $tresults = mysqli_query($db, "SELECT name FROM tbl_dept WHERE name='$cat_name'"
                 }
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<script type="text/javascript" src="../includes/tinymce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        element_format : "html",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+
+        // Skin options
+        skin : "o2k7",
+        skin_variant : "silver",
+
+        // Drop lists for link/image/media/template dialogs
+        template_external_list_url : "js/template_list.js",
+        external_link_list_url : "js/link_list.js",
+        external_image_list_url : "js/image_list.js",
+        media_external_list_url : "js/media_list.js",
+
+});
+</script>
+</head>
 <body>
 <div class="container ">
 
@@ -71,13 +108,17 @@ $tresults = mysqli_query($db, "SELECT name FROM tbl_dept WHERE name='$cat_name'"
 					<form method="post" action="<?php echo $PHP_SELF;?>"> 
 						<tr>
 <?php
-$nresults = mysqli_query($db, "SELECT name, topic FROM tbl_dept WHERE id='$id'");
+$nresults = mysqli_query($db, "SELECT name, id, description, topic FROM tbl_dept WHERE id='$id'");
         $nrow = mysqli_fetch_array($nresults);
         $cat_name=$nrow['name'];								
 		$id=$nrow['id'];								
-		$topic_id=$nrow['topic'];								
+		$topic_id=$nrow['topic'];	
+		$description=$nrow['description'];			
 ?>
                                 <td>New Category Name:</td><td><input type="text" name="cat_name" value="<?php echo $cat_name ?>" size="85"><span class="red"><?php echo $cat_error ?></span></td>
+                                </tr>
+								<tr>
+                                <td>Description:</td><td><textarea name="description" cols="70" rows="25" Value="<?php echo $description ?>"><?php echo $description ?></textarea></td><td><span class="red"><?php echo $desc_error ?></span></td>
                                 </tr>
                                 <tr>
                                 <td class="lastrow">
